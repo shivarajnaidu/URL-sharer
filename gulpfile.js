8f15caf3caf18666a62668525f06a1470ed4a655
shivarajnaidu <shivarajnaidu@gmail.com>
2017-09-17 16:56:57 +0530

    Added gulp configuration for production  build

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+'use strict';
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const cleanCSS = require('gulp-clean-css');
+const htmlReplace = require('gulp-html-replace');
+const htmlmin = require('gulp-htmlmin');
+// const uglify = require('gulp-uglify');
+const minify = require('gulp-minify');
+const rename = require('gulp-rename');
+
+gulp.task('scripts', () => {
+    return gulp.src('js/*.js')
+        .pipe(concat('app.js'))
+        .pipe(minify())
+        .pipe(rename('app.min.js'))
+        .pipe(gulp.dest('build/js'));
+});
+
+gulp.task('styles', () => {
+    return gulp.src('css/*.css')
+        .pipe(concat('app.css'))
+        .pipe(gulp.dest('build/css/'))
+        .pipe(cleanCSS())
+        .pipe(rename('app.min.css'))
+        .pipe(gulp.dest('build/css/'))
+});
+
+gulp.task('html', () => {
+    return gulp.src('index.html')
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(htmlReplace({
+            'styles': 'css/app.min.css',
+            'scripts': 'js/app.min.js'
+        }))
+        .pipe(gulp.dest('build'))
+});
+
+gulp.task('fonts', () => {
+    return gulp.src('fonts/*')
+        .pipe(gulp.dest('build/fonts'))
+});
+
+gulp.task('icons', () => {
+    return gulp.src('icons/*')
+        .pipe(gulp.dest('build/icons'))
+});
+
+gulp.task('mainfest', () => {
+    return gulp.src('mainfest.json')
+        .pipe(gulp.dest('build/mainfest.json'))
+});
+
+gulp.task('default', ['scripts', 'styles', 'html', 'fonts', 'icons', 'mainfest']);
