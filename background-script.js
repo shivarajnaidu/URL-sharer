e8c674d3b162a51d8758278e9bd704a79441f5a1
Yuvaraj V <shivarajnaidu@gmail.com>
2026-03-24 11:11:49 +0530

    chrome specific chnages to publish

--- background-script.js
+++ background-script.js
@@ -1,15 +1,22 @@
 'use strict';
 
-const openedTabIDs = [];
+async function getOpenedTabIDs() {
+    const data = await chrome.storage.session.get('openedTabIDs');
+    return data.openedTabIDs || [];
+}
+
+async function saveOpenedTabIDs(ids) {
+    await chrome.storage.session.set({ openedTabIDs: ids });
+}
 
 function isFacebook(tab) {
     const { url } = tab;
     return url && url.includes('facebook.com');
 }
 
-function isTabOpenedByMe(tab) {
-    const { id } = tab;
-    return openedTabIDs.includes(id);
+async function isTabOpenedByMe(tab) {
+    const ids = await getOpenedTabIDs();
+    return ids.includes(tab.id);
 }
 
 function isLoading(tab) {
@@ -22,24 +29,32 @@ const isEmptyTab = (tab) => {
     return (url === 'chrome://newtab/') || (url === 'about:blank');
 };
 
-function closeTab(id) {
+async function closeTab(id) {
     chrome.tabs.remove(id);
-    openedTabIDs.splice(openedTabIDs.indexOf(id), 1);
+    const ids = await getOpenedTabIDs();
+    ids.splice(ids.indexOf(id), 1);
+    await saveOpenedTabIDs(ids);
 }
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     if (request.type === 'sharer-tab-id') {
         const { id } = request.data;
-        openedTabIDs.push(id);
+        getOpenedTabIDs().then(ids => {
+            ids.push(id);
+            saveOpenedTabIDs(ids);
+        });
     }
 });
 
 chrome.tabs.onUpdated.addListener(onTabUpdateListener);
 
-function onTabUpdateListener(tabID, changeInfo, tab) {
-    if (isLoading(tab) || !isTabOpenedByMe(tab) || isEmptyTab(tab)) {
+async function onTabUpdateListener(tabID, changeInfo, tab) {
+    if (isLoading(tab) || isEmptyTab(tab)) {
         return;
     }
+
+    const isOpened = await isTabOpenedByMe(tab);
+    if (!isOpened) return;
 
     const { url, id } = tab;
 
